8dc82bf27a549fc2ebc0e937fc5ab8b30751ff3b
q191201771 <191201771@qq.com>
2019-08-24 12:47:37 +0800

    兼容srs-bench， rtmp message中的非首个 chunk 使用fmt3 而非 fmt0

--- pkg/rtmp/chunk_divider.go
+++ pkg/rtmp/chunk_divider.go
@@ -8,28 +8,15 @@ import (
 	"github.com/q191201771/lal/pkg/util/bele"
 )
 
-// TODO chef: 只使用fmt0格式的chunk，不参考前一个chunk的头字段
+// TODO chef: 新的message的第一个chunk始终使用fmt0格式，不参考前一个message
 func Message2Chunks(message []byte, header *Header, chunkSize int) []byte {
 	return message2Chunks(message, header, nil, chunkSize)
 }
 
-// TODO chef: 这里所有的chunk的格式判断是参考的前一个message的字段。实际上应该参考当前message的前一个chunk的字段吧？
-func message2Chunks(message []byte, header *Header, prevHeader *Header, chunkSize int) []byte {
-	//if header.CSID < minCSID || header.CSID > maxCSID {
-	//	return nil, rtmpErr
-	//}
-
-	// 计算chunk数量，最后一个chunk的大小
-	numOfChunk := len(message) / chunkSize
-	lastChunkSize := chunkSize
-	if len(message)%chunkSize != 0 {
-		numOfChunk++
-		lastChunkSize = len(message) % chunkSize
-	}
-
-	maxNeededLen := (chunkSize + maxHeaderSize) * numOfChunk
-	out := make([]byte, maxNeededLen)
-	index := 0
+// TODO chef: 返回值直接传入
+func calcHeader(header *Header, prevHeader *Header) []byte {
+	var index int
+	out := make([]byte, 12)
 
 	// 计算fmt和timestamp
 	fmt := uint8(0)
@@ -100,13 +87,34 @@ func message2Chunks(message []byte, header *Header, prevHeader *Header, chunkSiz
 		index += 4
 	}
 
+	return out[0:index]
+}
+
+func message2Chunks(message []byte, header *Header, prevHeader *Header, chunkSize int) []byte {
+	//if header.CSID < minCSID || header.CSID > maxCSID {
+	//	return nil, rtmpErr
+	//}
+
+	// 计算chunk数量，最后一个chunk的大小
+	numOfChunk := len(message) / chunkSize
+	lastChunkSize := chunkSize
+	if len(message)%chunkSize != 0 {
+		numOfChunk++
+		lastChunkSize = len(message) % chunkSize
+	}
+
+	maxNeededLen := (chunkSize + maxHeaderSize) * numOfChunk
+	out := make([]byte, maxNeededLen)
+
+	var index int
+
+	// NOTICE 和srs交互时，发现srs要求message中的非第一个chunk不能使用fmt0
 	// 将message切割成chunk放入chunk body中
-	headLen := index
 	for i := 0; i < numOfChunk; i++ {
-		if i != 0 {
-			copy(out[index:], out[0:headLen])
-			index += headLen
-		}
+		head := calcHeader(header, prevHeader)
+		copy(out[index:], head)
+		index += len(head)
+
 		if i != numOfChunk-1 {
 			copy(out[index:], message[i*chunkSize:i*chunkSize+chunkSize])
 			index += chunkSize
@@ -114,6 +122,7 @@ func message2Chunks(message []byte, header *Header, prevHeader *Header, chunkSiz
 			copy(out[index:], message[i*chunkSize:i*chunkSize+lastChunkSize])
 			index += lastChunkSize
 		}
+		prevHeader = header
 	}
 
 	return out[:index]
