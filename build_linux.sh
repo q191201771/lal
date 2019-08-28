ea29c1f2f3b4bf5040b0e9c925e0e101d84b40e5
q191201771 <191201771@qq.com>
2019-08-28 18:55:43 +0800

    build.sh: add GitStatus

--- build_linux.sh
+++ build_linux.sh
@@ -9,11 +9,18 @@ if [ ! -d ${ROOT_DIR}/bin ]; then
   mkdir bin
 fi
 
+#GitCommitID=`git log --pretty=format:'%h' -n 1`
+GitCommitLog=`git log --pretty=oneline -n 1`
+GitStatus=`git status -s`
+BuildTime=`date +'%Y.%m.%d.%H%M%S'`
+BuildGoVersion=`go version`
+
 cd ${ROOT_DIR}/app/lal && \
   GOOS=linux GOARCH=amd64 go build -ldflags " \
-    -X 'github.com/q191201771/lal/pkg/util/bininfo.GitCommitID=`git log --pretty=format:'%h' -n 1`' \
-    -X 'github.com/q191201771/lal/pkg/util/bininfo.BuildTime=`date +'%Y.%m.%d.%H%M%S'`' \
-    -X 'github.com/q191201771/lal/pkg/util/bininfo.BuildGoVersion=`go version`' \
+    -X 'github.com/q191201771/nezha/pkg/bininfo.GitCommitLog=${GitCommitLog}' \
+    -X 'github.com/q191201771/nezha/pkg/bininfo.GitStatus=${GitStatus}' \
+    -X 'github.com/q191201771/nezha/pkg/bininfo.BuildTime=${BuildTime}' \
+    -X 'github.com/q191201771/nezha/pkg/bininfo.BuildGoVersion=${BuildGoVersion}' \
   " -o ${ROOT_DIR}/bin/lal
 
 cd ${ROOT_DIR}/app/flvfile2es && GOOS=linux GOARCH=amd64 go build -o ${ROOT_DIR}/bin/flvfile2es
