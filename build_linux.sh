832c9dac10b161dbfeeb7a85f3591a8008923b4f
q191201771 <191201771@qq.com>
2019-09-03 16:23:01 +0800

    build_linux.sh 编译出的部分可执行程序文件名添加.linux后缀

--- build_linux.sh
+++ build_linux.sh
@@ -21,11 +21,11 @@ cd ${ROOT_DIR}/app/lal && \
     -X 'github.com/q191201771/nezha/pkg/bininfo.GitStatus=${GitStatus}' \
     -X 'github.com/q191201771/nezha/pkg/bininfo.BuildTime=${BuildTime}' \
     -X 'github.com/q191201771/nezha/pkg/bininfo.BuildGoVersion=${BuildGoVersion}' \
-  " -o ${ROOT_DIR}/bin/lal
+  " -o ${ROOT_DIR}/bin/lal.linux
 
 cd ${ROOT_DIR}/app/flvfile2es && GOOS=linux GOARCH=amd64 go build -o ${ROOT_DIR}/bin/flvfile2es
 
-cd ${ROOT_DIR}/app/flvfile2rtmppush && GOOS=linux GOARCH=amd64 go build -o ${ROOT_DIR}/bin/flvfile2rtmppush
+cd ${ROOT_DIR}/app/flvfile2rtmppush && GOOS=linux GOARCH=amd64 go build -o ${ROOT_DIR}/bin/flvfile2rtmppush.linux
 
 cd ${ROOT_DIR}/app/httpflvpull && GOOS=linux GOARCH=amd64 go build -o ${ROOT_DIR}/bin/httpflvpull
 
