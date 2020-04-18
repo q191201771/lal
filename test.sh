2660880ea19ac4638f9dc39e59617be8591992f9
q191201771 <191201771@qq.com>
2020-04-18 17:41:31 +0800

    [chore] wget test.flv from blog if doc repo fail

--- test.sh
+++ test.sh
@@ -38,8 +38,10 @@ if [ ! -f "./testdata/test.flv" ]; then
     if [ ! -d "./testdata" ]; then
         mkdir "./testdata"
     fi
-    #wget https://pengrl.com/images/other/source.200kbps.768x320.flv -O ./testdata/test.flv
     wget https://github.com/q191201771/doc/raw/master/stuff/source.200kbps.768x320.flv -O ./testdata/test.flv
+    if [ ! -f "./testdata/test.flv" ]; then
+        wget https://pengrl.com/images/other/source.200kbps.768x320.flv -O ./testdata/test.flv
+    fi
 fi
 if [ ! -f "./pkg/rtmp/testdata/test.flv" ]; then
     if [ ! -d "./pkg/rtmp/testdata" ]; then
