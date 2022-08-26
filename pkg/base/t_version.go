bcf2d32bf21ac356926f1164369de38f5f569049
q191201771 <191201771@qq.com>
2022-08-26 13:17:38 +0800

    [patch] update http-api version & http-notify version

--- pkg/base/t_version.go
+++ pkg/base/t_version.go
@@ -25,11 +25,11 @@ const ConfVersion = "v0.3.3"
 
 // HttpApiVersion lalserver的HTTP-API功能的版本号
 //
-const HttpApiVersion = "v0.3.3"
+const HttpApiVersion = "v0.4.0"
 
 // HttpNotifyVersion lalserver的HTTP-Notify功能的版本号
 //
-const HttpNotifyVersion = "v0.1.4"
+const HttpNotifyVersion = "v0.2.0"
 
 var (
 	LalLibraryName = "lal"
